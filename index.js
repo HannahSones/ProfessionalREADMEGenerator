3b20bf695fbb008da071eeea5f1fed2ff17fb483
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2020-11-17 21:52:58 +0000

    Inital JavaScript file created and template ReadME added

---
+++ index.js
@@ -0,0 +1,48 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+
+
+// Basic template for ReadME file
+`# Title
+## Description
+Add description here
+
+----------
+
+## Table of contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+----------
+
+## Installation
+Text added here.
+
+----------
+
+## Usage
+* When the Weather Dashboard is first loaded, the dashboard is empty and the forecast containers don't appear until a city has been searched.
+* Once a user searches for a cityleared, information from local storage is cleared, and the page is refreshed to display a new, blank page.
+
+-----------
+## License
+Choose a license from a list of options
+
+-------------
+## Contributing
+Text added here.
+
+--------------
+## Tests
+
+------------
+## Questions
+Github username is added with link to profile
+Email address
+`
